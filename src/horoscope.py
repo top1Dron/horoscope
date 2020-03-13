c0d3a036a6af6b73e62ff45e1c329f18d30572fe
top1Dron <pro100takvoin@gmail.com>
2020-03-13 17:32:02 +0200

    updated appending using capitalize()

--- src/horoscope.py
+++ src/horoscope.py
@@ -7,9 +7,9 @@ promises = ["гостей из забытого прошлого", "встреч
 
 generated_prophecies = []
 for i in range(0, 5):
-    prophecy = str(times[random.randrange(0, len(times))]) + \
+    prophecy = str(times[random.randrange(0, len(times))]).capitalize() + \
                 ' ' + str(advices[random.randrange(0, len(advices))]) + \
                 ' ' + str(promises[random.randrange(0, len(promises))]) + '.'
 
-    generated_prophecies.append(prophecy[0].title() + prophecy[1:])
+    generated_prophecies.append(prophecy)
     print(generated_prophecies[i])
