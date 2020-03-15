c0793744f135d014ee9e738901ec4b5483340a63
top1Dron <pro100takvoin@gmail.com>
2020-03-15 21:30:01 +0200

    Update strings on using f

---
+++ src/generate_index.py
@@ -0,0 +1,38 @@
+from src.horoscope import generateProphecies
+from datetime import datetime as dt
+
+
+def generatePage(head, body):
+    page = f"""<!DOCTYPE html>
+<html>
+    {head}
+    {body}
+</html>
+"""
+    return page
+
+
+def generateHead(title):
+    head = f"\t\t<meta charset='utf-8'>\n\t\t<title>{title}</title>\n"
+    return f"<head>\n{head}\t</head>"
+
+
+def generateBody(header, paragraphs):
+    body = f"\t\t<h1>{header}</h1>\n"
+    for p in paragraphs:
+        body += f"\t\t<p>{p}</p>\n"
+    return f"<body>\n{body}\t</body>"
+
+
+def savePage(title, header, paragraphs, output="index.html"):
+    file = open(output, 'w')
+    page = generatePage(head=generateHead(title),
+                        body=generateBody(header=header, paragraphs=paragraphs))
+    print(page, file=file)
+    file.close()
+
+
+today = dt.now().date()
+savePage(title="Гороскоп на сегодня",
+         header="Что день " + str(today) + " готовит",
+         paragraphs=generateProphecies())
--- src/horoscope.py
+++ src/horoscope.py
@@ -1,18 +1,18 @@
 import random
 
 
-def generate_prophecies(total_num=5):
+def generateProphecies(total_num=5):
     times = ["утром", "днём", "вечером", "ночью", "после обеда", "перед сном"]
     advices = ["ожидайте", "предостерегайтесь", "будьте открыты для"]
     promises = ["гостей из забытого прошлого", "встреч со старыми знакомыми",
                 "неожиданного праздника", "приятных перемен"]
 
-    generated_prophecies = []
+    generatedProphecies = []
     for i in range(0, 5):
         time = random.choice(times)
         advice = random.choice(advices)
         promise = random.choice(promises)
         prophecy = time.capitalize() + ' ' + advice + ' ' + promise + '.'
-        generated_prophecies.append(prophecy)
+        generatedProphecies.append(prophecy)
 
-    return generated_prophecies
+    return generatedProphecies
