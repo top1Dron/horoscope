9138e71f4d7ac12e3a32a294bf7acbb4496f084b
top1Dron <pro100takvoin@gmail.com>
2020-03-20 15:53:32 +0200

    Fixed server

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def api_forecasts():
 
 if __name__ == "__main__":
     if os.environ.get('APP_LOCATION') == 'heroku':
-        run(host='0.0.0.0', host = int(os.environ.get("PORT" ,5000)))
+        run(host='0.0.0.0', port = int(os.environ.get("PORT" ,5000)))
     else:
         run(
             host="localhost",
